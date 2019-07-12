ed5811315e7cafdfe921466a843fddfd0e64c225
Mioratahina <alymiratana@gmail.com>
2019-07-12 17:04:34 +0300

    projet vendredi 12 juillet

---
+++ 00_journalists.rb
@@ -0,0 +1,108 @@
+puts "Voici les listes de handle sur Twitter : "
+tab = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def nbr_journaliste(tab)
+	puts "le nombre de journaliste est : #{tab.length}"
+	return tab.length
+end
+
+def handle_numero(tab)
+	number=0
+	n=0
+	tab.each do |i|
+		i.each_char do |j|
+			if (j == '0' || j=='1' || j=='2' || j=='3' || j=='4' || j=='5' || j=='6' || j=='7' || j=='8' || j=='9')
+				n=1
+			end
+		end
+		if n==1
+			number += n
+		end
+		n=0
+	end
+	puts "Parmi ces noms, #{number} contiennent des chiffres"
+end
+
+def prenom_aude(tab)
+	nombre=0
+	c = Array.new()
+	tab.each do |i|
+		p=0
+		i.each_char do |j|
+		c[p] = j
+		p+=1
+		end	
+		for j in 0..c.length-3
+			if (c[j]=='a' or c[j]=='A')
+				if (c[j+1]=='u' or c[j+1]=='U')
+					if (c[j+2]=='d' or c[j+2]=='D')
+						if (c[j+3]=='e' or c[j+3]=='E')
+							t=1
+						end
+					end
+				end
+			end
+			if t==1
+				nombre += t
+				t=0
+			end
+		end
+	end
+puts "Le nombre de prenom qui contient AUDE est : #{nombre}"
+end
+
+def majuscule(tab)
+	total=0
+	j=1
+	for i in 0..tab.length-1
+		if (tab[i][j]=='A' || tab[i][j]=='B' || tab[i][j]=='C' || tab[i][j]=='D' || tab[i][j]=='E' || tab[i][j]=='F' || tab[i][j]=='G' || tab[i][j]=='H' || tab[i][j]=='I' || tab[i][j]=='J' || tab[i][j]=='K' || tab[i][j]=='L' || tab[i][j]=='M' || tab[i][j]=='N' || tab[i][j]=='O' || tab[i][j]=='P' || tab[i][j]=='Q' || tab[i][j]=='R' || tab[i][j]=='S' || tab[i][j]=='T' || tab[i][j]=='U' || tab[i][j]=='V' || tab[i][j]=='W' || tab[i][j]=='X' || tab[i][j]=='Y' || tab[i][j]=='Z')
+			m=1
+		end
+		if m==1
+			total+=1
+			m=0
+		end
+	end
+	puts "Le nombre de prenom qui commence par un majuscule est : #{total}"
+end
+
+def majftsn(tab)
+	nnn=0
+	n1=0
+	tab.each do |i|
+		i.each_char do |j|
+			if (j=='A' || j=='B' || j=='C' || j=='D' || j=='E' || j=='F' || j=='G' || j=='H' || j=='I' || j=='J' || j=='K' || j=='L' || j=='M' || j=='N' || j=='O' || j=='P' || j=='Q' || j=='R' || j=='S' || j=='T' || j=='U' || j=='V' || j=='W' || j=='X' || j=='Y' || j=='Z')
+				n1=1
+			end
+		end
+		if n1==1
+			nnn += n1
+		end
+		n1=0
+	end
+	puts "Le nombre de prenom qui contient un majuscule est : #{nnn}"
+end
+
+def underscor(tab)
+	num=0
+	n2=0
+	tab.each do |i|
+		i.each_char do |j|
+			if (j == '_')
+				n2=1
+			end
+		end
+		if n2==1
+			num += n2
+		end
+		n2=0
+	end
+	puts "Le nombre de prenom qui contient un underscore est : #{num}"
+end
+
+nbr_journaliste(tab)
+handle_numero(tab)
+prenom_aude(tab)
+majuscule(tab)
+majftsn(tab)
+underscor(tab)
